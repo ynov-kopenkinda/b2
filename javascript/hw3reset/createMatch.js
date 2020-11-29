1b931b3d3a151c2f3777f6c1b9726b269b130480
Kopenkin Dmitrii <kopenkin.da@gmail.com>
2020-11-29 22:51:23 +0500

    Update createMatch.js

--- javascript/hw3reset/createMatch.js
+++ javascript/hw3reset/createMatch.js
@@ -1,12 +1,3 @@
-// module.exports = [
-//   {
-//     homeTeam: 'Paul Mourgues',
-//     visitorTeam: 'Test Delete',
-//     score: [2, 2],
-//     date: '2020-12-03T00:00:00Z'
-//   }
-// ]
-
 module.exports = (homeTeam, visitorTeam, score, date) => ({
   homeTeam,
   visitorTeam,
